32c365237fddbde22866646756ac6c61112d55e8
DarkIlyas <ilyas.bouziane@um5s.net.ma>
2020-03-11 22:50:46 +0100

    started spring app

---
+++ coronavirus-tracker/src/test/java/com/corona/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corona.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
