90c193749f52d9d96cf98d083910bc5aee487f52
DarkIlyas <ilyas.bouziane@um5s.net.ma>
2020-03-13 18:10:04 +0100

    builded new created spring project + add java csv library

---
+++ coronavirus-tracker/src/main/java/com/corona/coronavirustracker/services/CoronaDataService.java
@@ -0,0 +1,13 @@
+package com.corona.coronavirustracker.services;
+
+import sun.net.www.http.HttpClient;
+
+public class CoronaDataService {
+
+    private static String CORONA_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+
+    public void fetchCoronaData(){
+        HttpClient client = HttpClient.
+
+    }
+}
