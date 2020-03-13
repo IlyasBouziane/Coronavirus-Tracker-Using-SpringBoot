483d60c6d9a2e0c6243706ea81c4cf8f83c31586
DarkIlyas <ilyas.bouziane@um5s.net.ma>
2020-03-13 18:25:51 +0100

    converted to jdk 11.0.6 and tested successfully data fetching

--- coronavirus-tracker/src/main/java/com/corona/coronavirustracker/services/CoronaDataService.java
+++ coronavirus-tracker/src/main/java/com/corona/coronavirustracker/services/CoronaDataService.java
@@ -1,13 +1,28 @@
 package com.corona.coronavirustracker.services;
 
-import sun.net.www.http.HttpClient;
 
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+@Service
 public class CoronaDataService {
 
     private static String CORONA_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
 
-    public void fetchCoronaData(){
-        HttpClient client = HttpClient.
-
+    @PostConstruct
+    public void fetchCoronaData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(CORONA_DATA_URL))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
